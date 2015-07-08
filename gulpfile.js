cb652fc405ac8dec9d350d68de7878adb89d54f9
Austin Burdine <acburdine@gmail.com>
2015-07-08 12:12:52 -0400

    refactor development and production flows
    closes #12
    - removes dependencies from project
    - refactors gulp usage to have separate dev and auth flows
    - adds gulp "release" command to make a production zip file

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,108 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    path = require('path'),
-    wrap = require('gulp-wrap'),
-    concat = require('gulp-concat'),
-    declare = require('gulp-declare'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
-    color = require('colors');
+/*
+ * Gulp Build File for Adjure
+ *
+ * This file contains all of the build tasks for the adjure program
+ * To see a more readable list of tasks, run `gulp help`
+ * 
+ */
 
-var lessWatchPath = [
-		'./less/**/*.less'
-	],
-	lessCompilePath = [
-		'./less/screen.less'
-	],
-	lessDestPath = './assets/css';
+var del      = require('del'),
+    zip      = require('gulp-zip'),
+    path     = require('path'),
+    chalk    = require('chalk'),
+    less     = require('gulp-less'),
+    concat   = require('gulp-concat'),
+    uglify   = require('gulp-uglify'),
+    sequence = require('run-sequence'),
 
-function lessError(err) {
-	if (err.plugin == "gulp-less") {
-		console.log("######################".red);
-		console.log("##    LESS ERROR    ##".red);
-		console.log("######################".red);
-		console.log("");
-		console.log(err.message);
-		console.log("");
-		console.log("File: ".green + err.fileName);
-		console.log("Line: ".green + err.line);
-		console.log("");
-		console.log(err.extract.toString().blue);
-		console.log("");
-		console.log("");
-	} else {
-		console.log(err.toString());
-	}
+    gulp     = require('gulp-help')(require('gulp'), {
+        hideEmpty: true,
+        hideDepsMessage: true
+    }),
+
+    paths    = {
+        js: ['./bower_components/jquery/dist/jquery.js', './bower_components/handlebars/handlebars.js', './bower_components/handlebars/handlebars.runtime.js', './src/js/**/*.js'],
+        css: ['./src/less/adjure.less']
+    };
+
+/*
+ * Error Handling Function
+ */
+function error(err) {
+    var file = err.fileName || err.file || 'Not specified',
+        line = err.lineNumber || err.line || 'Not specified',
+        message = err.message || 'Not specified';
+
+    console.log(chalk.red('An error occured in the ' + err.plugin + ' plugin.'));
+    console.log(chalk.red('with the message: ') + message);
+    console.log(chalk.green('File: ') + file);
+    console.log(chalk.green('Line: ') + line);
+    console.log('');
+    console.log(chalk.green('Raw Error: ') + err.toString());
+
+    this.emit('end');
 }
 
-gulp.task('less', function () {
-	gulp.src(lessCompilePath).on('error', lessError)
-		.pipe(less({
-			paths: [ path.join(__dirname, 'less', 'includes') ]
-		})).on('error', lessError)
-		.pipe(gulp.dest(lessDestPath)).on('error', lessError);
+/*
+ * Build Tasks
+ */
+
+// Development Build Task - compiles all files into developer-ready format
+gulp.task('dev', 'Compiles all code in uncompressed format', function (cb) {
+    sequence('clean:assets', ['js', 'css'], cb);
 });
 
-gulp.task('dependencies', function (){
-    gulp.src('./bower_components/handlebars/handlebars.js')
-        .pipe(gulp.dest("./assets/vendor/"));
-    gulp.src('./bower_components/handlebars/handlebars.runtime.js')
-        .pipe(gulp.dest("./assets/vendor/"));
-    gulp.src('./bower_components/jquery/dist/jquery.js')
-        .pipe(gulp.dest("./assets/vendor/"));
+// Production Build Task - compiles all files into production-ready format
+gulp.task('prod', 'Compiles all code in compressed format', function (cb) {
+    sequence('clean:assets', ['js:min', 'css'], cb);
 });
 
-gulp.task('watch', function() {
-	gulp.watch(lessWatchPath, ['less']);
+// Regular JS build task - compiles and minifies all vendor and adjure js into /assets/adjure.js
+gulp.task('js', function () {
+    var bower = './bower_components/';
+    return gulp.src(paths.js)
+        .pipe(concat('adjure.js'))
+        .pipe(gulp.dest('./assets/'));
 });
 
-gulp.task('default', ['less']);
+// Min JS build task - compiles and minifies all vendor and adjure js into /assets/adjure.js
+gulp.task('js:min', function () {
+    return gulp.src(paths.js)
+        .pipe(concat('adjure.js'))
+        .pipe(uglify({mangle: true}))
+        .pipe(gulp.dest('./assets/'));
+})
+
+// CSS Build task - compiles the less src files into /assets/adjure.css
+gulp.task('css', function () {
+    return gulp.src(paths.css)
+        .pipe(less()).on('error', error)
+        .pipe(gulp.dest('./assets/'));
+});
+
+// Watch Task - watches for changes in src files and rebuilds them on file change
+gulp.task('watch', 'Builds files in dev mode and then watches for changes', ['dev'], function () {
+    gulp.watch('./src/less/**/*.less', ['css']);
+    gulp.watch('./src/js/**/*.js', ['js'])
+});
+
+// Release task - compresses all necessary files into dist/adjure.zip
+gulp.task('release', 'Creates a release zip', ['prod'], function () {
+    return gulp.src(['./assets/*', './adjure.html'], {base: './'})
+        .pipe(zip('adjure.zip'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+/*
+ * Cleanup/Removal Tasks
+ */
+
+// Clean Task - removes all installed dependencies
+gulp.task('clean', 'Removes all dependencies', function (cb) {
+    del(['./bower_components', './node_modules'], cb);
+});
+
+// Assets Clean Task - removes all compiled assets
+gulp.task('clean:assets', function (cb) {
+    del('./assets/**/*', cb);
+});
